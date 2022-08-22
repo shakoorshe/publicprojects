39481b8cef88a0ab3ac092c71e6bca5a6bba353e
shakoorshe <mattsy4c@gmail.com>
2022-08-22 14:14:25 -0400

    PICKS>PY

--- spares/picks.py
+++ spares/picks.py
@@ -1,4 +1,15 @@
 import random
-
-
-picks = random.randint(3, 9)
+n = random.randint(1, 99)
+guess = int(input("Enter an integer from 1 to 99: "))
+while n != "guess":
+    print
+    if guess < n:
+        print ("guess is low")
+        guess = int(input("Enter an integer from 1 to 99: "))
+    elif guess > n:
+        print ("guess is high")
+        guess = int(input("Enter an integer from 1 to 99: "))
+    else:
+        print ("you guessed it!")
+        break
+    print
