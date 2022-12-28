2efede2f51b990b740f1531fb0cd410d18f56245
shakoorshe <mattsy4c@gmail.com>
2022-12-28 11:11:26 -0500

    A

--- spares/learn swift/arrays.swift
+++
@@ -1,5 +0,0 @@
-// Arrays index at 0
-var ages = [21,15,99,100,5,20]
-
-// empty array
-ages.count
--- spares/learn swift/hello.swift
+++
@@ -1,6 +0,0 @@
-import UIkit
-var highScore = 0
-highScore = 55
-let myName = "Matthew"
-var currentActiveUsers = 124
-currentActiveUsers = 100000000
