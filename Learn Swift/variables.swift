939c8e88521f150aa4a755e8a24a939ae0d564ca
shakoorshe <mattsy4c@gmail.com>
2022-12-28 11:36:47 -0500

    A

---
+++ Learn Swift/variables.swift
@@ -0,0 +1,9 @@
+// How to declare an integer 
+var highScore: Int = 0
+// how to delcare a float(6 decimal places) and double(15 decimal places)
+var decimal: float = 0.76
+var percent: double = 0.7764383
+//how to delcare a string
+var myName: string = "Sheik"
+// how to declare a boolean
+var memeSolid: boolean: true
