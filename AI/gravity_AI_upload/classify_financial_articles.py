a5f4380d47c69d7705617168e616b7433545fbe7
shakoorshe <mattsy4c@gmail.com>
2023-01-21 12:18:30 -0500

    A

---
+++ AI/gravity_AI_upload/classify_financial_articles.py
@@ -0,0 +1,7 @@
+from gravityai import gravityai as grav
+import pickle
+import pandas as pd
+
+model = pickle.load(open(""))
+tfidf_vectorizer = pickle.load(open(""))
+label_encoder = pickle.load(open(""))
