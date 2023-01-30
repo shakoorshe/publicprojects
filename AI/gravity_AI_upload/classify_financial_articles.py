49dc5f1b89b8da786ebd38e5ed436a7ab394093c
shakoorshe <mattsy4c@gmail.com>
2023-01-30 17:19:02 -0500

    1/30/23

--- AI/gravity_AI_upload/classify_financial_articles.py
+++ AI/gravity_AI_upload/classify_financial_articles.py
@@ -2,6 +2,23 @@ from gravityai import gravityai as grav
 import pickle
 import pandas as pd
 
-model = pickle.load(open(""))
-tfidf_vectorizer = pickle.load(open(""))
-label_encoder = pickle.load(open(""))
+model = pickle.load(open("financial_text_classifier.pkl", "rb"))
+tfidf_vectorizer = pickle.load(open("financial_text_vectorizer.pkl", "rb"))
+label_encoder = pickle.load(open("financial_text_encoder.pkl", "rb"))
+
+
+def process(inPath, outPath):
+    # Read the input file
+    input_df = pd.read_csv(inPath)
+    # vectorize the data
+    features = tfidf_vectorizer.transform(input_df["body"])
+    # predict the classes
+    predictions = model.predict(features)
+    # convert output labels to categories
+    input_df["category"] = label_encoder.inverse_transform(predictions)
+    # save results to a csv file
+    output_df = input_df[["id", "category"]]
+    output_df.to_csv(outPath, index=False)
+
+
+grav.wait_for_requests(process)
